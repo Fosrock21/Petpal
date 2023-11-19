b1f3a64c1dbd65367931887a205f5259b2b34448
yourusername <114538241+Fosrock21@users.noreply.github.com>
2023-11-19 18:45:22 -0500

    pet page views done

--- server.js
+++ server.js
@@ -38,9 +38,33 @@ app.get('/display-shelters', (req, res) => {
     });
 });
 
-// Fetch and display Application Details
-app.get('/applicationDetails', (req, res) => {
-    const sqlQuery = 'SELECT * FROM View_ApplicationDetails';
+// Fetch and display Adoption Status(view7)
+app.get('/adoptionStatus', (req, res) => {
+    const sqlQuery = 'SELECT * FROM view_adoptionstatus';
+    db.query(sqlQuery, (err, results) => {
+        res.json(results);
+    });
+});
+
+// Fetch and display The pet and their current shelter(view8)
+app.get('/Petshelters', (req, res) => {
+    const sqlQuery = 'SELECT * FROM view_petshelters';
+    db.query(sqlQuery, (err, results) => {
+        res.json(results);
+    });
+});
+
+// Fetch and display Accessory Availability by Pet Type(view9)
+app.get('/accessoriesForPets', (req, res) => {
+    const sqlQuery = 'SELECT * FROM view_AccessoriesForPets';
+    db.query(sqlQuery, (err, results) => {
+        res.json(results);
+    });
+});
+
+// Fetch and display the pet and their latest aplication date(view3)
+app.get('/latestApplicationDate', (req, res) => {
+    const sqlQuery = 'SELECT * FROM view_PetLatestApplication';
     db.query(sqlQuery, (err, results) => {
         res.json(results);
     });
